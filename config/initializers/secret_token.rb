013bfc5a3e90b27890eb1ffe82eadfacd0381c37
Marc Bungart <dev@webrails.de>
2013-03-25 21:46:16 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloog::Application.config.secret_key_base = 'cc8006661882361fea7cfedb6188f1aa120d28b574200a98724b3d95e57e0b29939e6543bd195da40efe50f752fefa731f8399f1eb0dbd7ddaaf8265737048fa'
