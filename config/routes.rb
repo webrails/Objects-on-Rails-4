8b681624f24a29e654e5890d18a41ea1eeaf3307
Marc Bungart <dev@webrails.de>
2013-03-31 13:01:31 +0200

    Submitting posts (fix typo)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Bloog::Application.routes.draw do
 
-  resoures :posts
+  resources :posts
 
   get "blog/index"
   # The priority is based upon order of creation: first created -> highest priority.
