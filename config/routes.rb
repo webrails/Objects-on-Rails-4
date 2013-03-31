383f00167db2fe0f67c0bedb6414234e5c22cc67
Marc Bungart <dev@webrails.de>
2013-03-31 12:56:49 +0200

    Submitting posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Bloog::Application.routes.draw do
+
+  resoures :posts
+
   get "blog/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
